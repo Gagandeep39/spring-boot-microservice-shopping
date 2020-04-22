b5a8d48b175095396843e4dae36464d844404652
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-22 22:49:38 +0530

    Added shopping cart service

---
+++ shopping-cart-service/src/test/java/com/gagan/shoppingcartservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagan.shoppingcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
