93721694688a0e86f75644dafaf236fa0e98143b
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-22 22:44:40 +0530

    Maked the Server as Eureka server Added Comments Added configs

--- shopping-eureka-server/src/main/java/com/gagan/shoppingeurekaserver/DemoApplication.java
+++ shopping-eureka-server/src/main/java/com/gagan/shoppingeurekaserver/DemoApplication.java
@@ -2,8 +2,13 @@ package com.gagan.shoppingeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Make this application as discovery server 
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
