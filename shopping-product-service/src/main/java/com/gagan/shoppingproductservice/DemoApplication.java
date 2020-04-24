3db6d5477a93e067cece7c74d3422cde99448a77
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-24 12:55:03 +0530

    Added Logger using AOP

--- shopping-product-service/src/main/java/com/gagan/shoppingproductservice/DemoApplication.java
+++ shopping-product-service/src/main/java/com/gagan/shoppingproductservice/DemoApplication.java
@@ -3,12 +3,14 @@ package com.gagan.shoppingproductservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@SpringBootApplication
-@EnableEurekaClient
-@EnableSwagger2WebMvc
+@SpringBootApplication	// Mark it as Pring boot
+@EnableEurekaClient	// Enables app to fetch registry from dicovery server
+@EnableSwagger2WebMvc	//Enable swagger
+// @EnableAspectJAutoProxy	// Enable logging, can be replaced with spring.aop.auto
 public class DemoApplication {
 
 
